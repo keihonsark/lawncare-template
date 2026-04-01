a0b201f47ca1893648814b42cb581a4199c142a3
keihonsark <keihonsark@gmail.com>
2026-04-01 14:34:50 -0700

    Center hero content, fix cycling text shift

---
+++ fix-hero-shift2.mjs
@@ -0,0 +1,7 @@
+﻿import { readFileSync, writeFileSync } from 'fs'
+let css = readFileSync('src/components/Hero.css', 'utf8')
+css = css.replace('display: block;', 'display: inline-block;')
+css = css.replace('.hero__content {', '.hero__content {\n  text-align: center;')
+css = css.replace('align-items: flex-start;', 'align-items: center;')
+writeFileSync('src/components/Hero.css', css, 'utf8')
+console.log('Fixed')
