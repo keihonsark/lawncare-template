6c9b13d83da8e3155777ed88d208c85c31714dae
keihonsark <keihonsark@gmail.com>
2026-04-01 14:44:50 -0700

    Center hero sub text

---
+++ fix-hero-clean.mjs
@@ -0,0 +1,5 @@
+﻿import { readFileSync, writeFileSync } from 'fs'
+let css = readFileSync('src/components/Hero.css', 'utf8')
+css = css.replace('max-width: 520px;', 'max-width: 520px;\n  margin-left: auto;\n  margin-right: auto;')
+writeFileSync('src/components/Hero.css', css, 'utf8')
+console.log('Fixed')
