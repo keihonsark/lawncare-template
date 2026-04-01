8b689b811645732aa0bf6d39bcbade30e99216e0
keihonsark <keihonsark@gmail.com>
2026-04-01 14:42:10 -0700

    Force hero content full width to stop layout shift

---
+++ fix-hero-width.mjs
@@ -0,0 +1,5 @@
+﻿import { readFileSync, writeFileSync } from 'fs'
+let css = readFileSync('src/components/Hero.css', 'utf8')
+css = css.replace('text-align: center;', 'text-align: center;\n  width: 100%;')
+writeFileSync('src/components/Hero.css', css, 'utf8')
+console.log('Fixed')
